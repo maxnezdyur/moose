d0d3d5c26568ca9c92b6e5dd610443843ab25b6b
moosetest <bounces@inl.gov>
2014-03-07 17:28:19 -0700

    Merging Modules into MOOSE #2460

--- modules/combined/src/main.C
+++ modules/combined/src/main.C
@@ -1,11 +1,11 @@
-#include "FluidMassEnergyBalanceApp.h"
+#include "ContactApp.h"
 #include "MooseInit.h"
 #include "Moose.h"
 #include "MooseApp.h"
 #include "AppFactory.h"
 
 // Create a performance log
-PerfLog Moose::perf_log("FluidMassEnergyBalance");
+PerfLog Moose::perf_log("Contact");
 
 // Begin the main program.
 int main(int argc, char *argv[])
@@ -14,10 +14,10 @@ int main(int argc, char *argv[])
   MooseInit init(argc, argv);
 
   // Register this application's MooseApp and any it depends on
-  FluidMassEnergyBalanceApp::registerApps();
+  ContactApp::registerApps();
 
   // This creates dynamic memory that we're responsible for deleting
-  MooseApp * app = AppFactory::createApp("FluidMassEnergyBalanceApp", argc, argv);
+  MooseApp * app = AppFactory::createApp("ContactApp", argc, argv);
 
   // Execute the application
   app->run();
