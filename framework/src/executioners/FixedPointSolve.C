cf57d2b9247b3e6db9d3ac767f9a20810f1af0b5
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-01-04 08:49:34 -0700

    Add fixed point begin/end execution flags and execution points

--- framework/src/executioners/FixedPointSolve.C
+++ framework/src/executioners/FixedPointSolve.C
@@ -470,6 +470,8 @@ FixedPointSolve::solveStep(Real & begin_norm,
     return false;
   }
 
+  _problem.execute(EXEC_FIXED_POINT_END);
+  _problem.execTransfers(EXEC_FIXED_POINT_END);
   _executioner.postSolve();
 
   if (_has_fixed_point_its && _has_fixed_point_norm)
