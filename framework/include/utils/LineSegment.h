bbef6f3ef298af4b8e3570cb3f6d2123905365bc
maxnezdyur <maxnezdyur@gmail.com>
2023-03-16 08:28:09 -0400

    add point to constant reporter

--- framework/include/utils/LineSegment.h
+++ framework/include/utils/LineSegment.h
@@ -94,7 +94,7 @@ private:
 };
 
 void dataStore(std::ostream & stream, LineSegment & l, void * context);
-void dataLoad(std::ostream & stream, LineSegment & l, void * context);
+void dataLoad(std::istream & stream, LineSegment & l, void * context);
 
 void to_json(nlohmann::json & json, const Point & p);
 void to_json(nlohmann::json & json, const LineSegment & l);
