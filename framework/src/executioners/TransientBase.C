0063b9805171eab448ed0a65991a7de87e24adc2
maxnezdyur <maxnezdyur@gmail.com>
2025-04-17 08:19:15 -0600

    update advance state for explicit methods

--- framework/src/executioners/TransientBase.C
+++ framework/src/executioners/TransientBase.C
@@ -392,6 +392,11 @@ TransientBase::incrementStepOrReject()
 void
 TransientBase::advanceState()
 {
+  const auto & tis = getTimeIntegrators();
+  for (auto & ti : tis)
+    // We do not want to advance state all if this is an explicit time integrator
+    if (ti->controlsState())
+      return;
   _problem.advanceState();
 }
 
